a9cfb93282977ecbbed301a320e8b0f3c23c8d9a
brajpatel <brajeshpatel0002@gmail.com>
2023-09-09 10:46:16 +0100

    Create posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -5,4 +5,20 @@ router.get('/', (req, res) => {
     res.render('index', { title: 'Homepage with some posts' });
 })
 
+router.get('/create', (req, res) => {
+    res.send('post create - GET')
+})
+
+router.post('/create', (req, res) => {
+    res.send('post create - POST')
+})
+
+router.get('/delete', (req, res) => {
+    res.send('post delete - GET');
+})
+
+router.post('/delete', (req, res) => {
+    res.send('post delete - POST')
+})
+
 module.exports = router;
