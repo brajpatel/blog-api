55fd502dae79d29dcdea9c1588f6422b7ae5c349
brajpatel <brajeshpatel0002@gmail.com>
2023-09-09 10:50:37 +0100

    Use login router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
+const loginRouter = require('./routes/login');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
