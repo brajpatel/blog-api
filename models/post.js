ecd94eae70b44bb26e84c49dd7fd7a7186497811
brajpatel <brajeshpatel0002@gmail.com>
2023-09-10 11:51:48 +0100

    Create posts model

--- models/post.js
+++ models/post.js
@@ -1,1 +1,13 @@
-// posts model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    date_added: { type: String, required: true },
+    published: { type: Boolean, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+})
+
+module.exports = mongoose.model('Post', PostSchema);
