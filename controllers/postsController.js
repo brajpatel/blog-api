329f8044e318892b0a8d5fb6e996ec8a596befa3
brajpatel <brajeshpatel0002@gmail.com>
2023-09-11 17:04:10 +0100

    Organise post routes

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,14 +1,16 @@
-const Posts = require('../models/post');
+const Post = require('../models/post');
 const asyncHandler = require('express-async-handler');
 
 exports.posts_list = asyncHandler(async (req, res, next) => {
-    const allPosts = await Posts.find().exec();
+    const allPosts = await Post.find().exec();
 
     res.json(allPosts);
 });
 
 exports.post_detail = asyncHandler(async (req, res, next) => {
-    const selectedPost = await Posts.find(req.params.id).populate("comments").exec();
+    const selectedPost = await Post.find(req.params.id).populate("comments").exec();
 
     res.json(selectedPost);
 });
+
+exports.post_create_post = [];
--- routes/posts.js
+++ routes/posts.js
@@ -4,22 +4,10 @@ const postsController = require('../controllers/postsController');
 
 router.get('/', postsController.posts_list);
 
-router.get('/:id', postsController.post_detail)
-
-router.get('/create', (req, res) => {
-    res.send('post create - GET')
-})
+router.get('/:id', postsController.post_detail);
 
 router.post('/create', (req, res) => {
     res.send('post create - POST')
-})
-
-router.get('/delete', (req, res) => {
-    res.send('post delete - GET');
-})
-
-router.post('/delete', (req, res) => {
-    res.send('post delete - POST')
 })
 
 module.exports = router;
