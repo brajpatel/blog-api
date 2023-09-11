d7271278d3bffe88f58775f99e72979fe074848e
brajpatel <brajeshpatel0002@gmail.com>
2023-09-11 16:52:02 +0100

    Add property to comment model to reference its post

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
     name: { type: String, minLength: 3, required: true },
     text: { type: String, minLength: 5, required: true },
-    date_added: { type: String, required: true }
+    date_added: { type: String, required: true },
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true}
 })
 
 module.exports = mongoose.model('Comment', CommentSchema);
