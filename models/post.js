5872e34fa7f7647777b5f32779c781e38a747191
brajpatel <brajeshpatel0002@gmail.com>
2023-09-17 09:47:40 +0100

    Change user property to author

--- models/post.js
+++ models/post.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
+    author: { type: String, minLength: 3, required: true },
     title: { type: String, minLength: 8, required: true },
     content: { type: String, minLength: 40, required: true },
     date_added: { type: String, required: true },
     image: { type: String, required: true },
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
 })
 
