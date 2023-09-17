342dba8bed1d009520888669b88d6b9bf74eb5f1
brajpatel <brajeshpatel0002@gmail.com>
2023-09-17 09:42:54 +0100

    Create new posts from post route

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -13,4 +13,22 @@ exports.post_detail = asyncHandler(async (req, res, next) => {
     res.json(selectedPost);
 });
 
-exports.post_create_post = [];
+exports.post_create_post = asyncHandler(async (req, res, next) => {
+    const post = new Post({
+        author: req.body.author,
+        title: req.body.title,
+        content: req.body.content,
+        date_added: new Date().toLocaleDateString() + ' - ' + new Date().toLocaleTimeString(),
+        image: req.body.image,
+        comments: []
+    })
+    
+    const postExists = await Post.findOne({ title: req.body.title });
+
+    if(postExists) {
+        return;
+    }
+    else {
+        await post.save();
+    }
+});
