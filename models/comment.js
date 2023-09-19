9a0e984f9cdbed89d7a62b2f99b65f590501fad1
brajpatel <brajeshpatel0002@gmail.com>
2023-09-19 13:23:13 +0100

    Rename text property to message

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
     name: { type: String, minLength: 3, required: true },
-    text: { type: String, minLength: 5, required: true },
+    message: { type: String, minLength: 20, required: true },
     date_added: { type: String, required: true },
     post: { type: Schema.Types.ObjectId, ref: "Post", required: true}
 })
