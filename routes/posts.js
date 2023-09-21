e6300762da799e3b73bcd93fae61700bbf3a9c8f
brajpatel <brajeshpatel0002@gmail.com>
2023-09-21 12:00:44 +0100

    Move post create route to top of post routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/postsController');
 
-router.get('/', postsController.posts_list);
+router.post('/create', postsController.post_create);
 
 router.get('/:id', postsController.post_detail);
 
 router.post('/:id', postsController.post_add_comment);
 
-router.post('/create', postsController.post_create);
+router.get('/', postsController.posts_list);
 
 module.exports = router;
